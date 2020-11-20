137d6d9f9a4c8fe65f569f2c6a5c301a6e50d4cd
MaiXiaoMeng <maikaiyang@outlook.com>
2020-11-20 13:24:57 +0800

    feat: 新增 监听并且修改 Request Headers 数据

--- scripts/background.js
+++ scripts/background.js
@@ -1,31 +1,35 @@
 ﻿CONFIG = get_config();
 REQUEST_HEADERS = CONFIG['request_headers'];
 
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    function (details) {
-        REQUEST_HEADERS.forEach(_REQUEST_HEADERS => {
-            if (get_matching(_REQUEST_HEADERS.url, details.url) && _REQUEST_HEADERS.type == details.type) {
-                _REQUEST_HEADERS.headers.forEach(_headers => {
-                    console.log(`URL:${details.url} EDIT HEADERS => NAME:${_headers.name} VALUE:${_headers.value}`);
-                    if (details.requestHeaders.some(item => item.name == _headers.name)) {
-                        for (let index = 0; index < details.requestHeaders.length; index++) {
-                            if (details.requestHeaders[index].name == _headers.name) {
-                                details.requestHeaders[index].value = _headers.value;
-                            }
-                        }
-                    } else {
-                        details.requestHeaders.push(_headers)
-                    }
-                })
-            }
-        });
-        return { requestHeaders: details.requestHeaders };
-    }, { urls: ['<all_urls>'] }, ["blocking", "requestHeaders"]
-);
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    chrome.tabs.sendMessage(sender.tab.id, {
-        ecomtool_response: true,
-        ust: 2,
-    });
+    chrome.tabs.sendMessage(sender.tab.id, {});
 });
+
+// 如果需要监听同步信息的话,监听器必须是同步的,不能是回调函数
+// 实际上监听只异步请求只需要调用一次
+(() => {
+    if (typeof listener == 'undefined') {
+        listener = function (details) {
+            REQUEST_HEADERS.forEach(_REQUEST_HEADERS => {
+                if (get_matching(_REQUEST_HEADERS.url, details.url) && _REQUEST_HEADERS.type.some(item => item == details.type)) {
+                    _REQUEST_HEADERS.headers.forEach(_headers => {
+                        console.log(`URL:${details.url} EDIT HEADERS => NAME:${_headers.name} VALUE:${_headers.value}`);
+                        if (details.requestHeaders.some(item => item.name == _headers.name)) {
+                            for (let index = 0; index < details.requestHeaders.length; index++) {
+                                if (details.requestHeaders[index].name == _headers.name) {
+                                    details.requestHeaders[index].value = _headers.value;
+                                }
+                            }
+                        } else {
+                            details.requestHeaders.push(_headers)
+                        }
+                    })
+                }
+            });
+            return { requestHeaders: details.requestHeaders };
+        };
+    }
+    chrome.webRequest.onBeforeSendHeaders.addListener(listener,
+        { "urls": ['<all_urls>'] },
+        ["extraHeaders", "requestHeaders", "blocking"]);
+})()
